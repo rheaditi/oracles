57087a4a064273cf9daa8171086b5c0edb8e5deb
Aditi Mohanty <rheaditi@users.noreply.github.com>
2016-10-23 19:11:43 +0530

    Simple servlet works

--- src/com/tempjsfree/Servlets/PieServlet.java
+++ src/com/tempjsfree/Servlets/PieServlet.java
@@ -1,7 +1,6 @@
-package com.temp.jsfree.Servlets;
+package com.tempjsfree.Servlets;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +10,6 @@ import java.io.PrintWriter;
 /**
  * Created by SG0226685 on 023, 23 Oct 2016.
  */
-@WebServlet(name = "PieServlet")
 public class PieServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
